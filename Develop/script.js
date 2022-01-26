3453a5d2a7cb4fe39a2255c0f14552cc6e55b40d
Jaxon Adams <jadamsresume1@gmail.com>
2022-01-26 15:22:09 -0700

    site will now validate responses to prompts

--- Develop/script.js
+++ Develop/script.js
@@ -5,14 +5,15 @@
   // Generate Password Based on Criteria
   // Display Password
 
+
 var promptCriteria = function() {
   window.alert("Please select the criteria you would like to meet for this password.");
 
   // Prompt Length
   var promptedLength = function() {
-    // Prompt length, and keep prompting if a null response is given
+    // Prompt length, and keep prompting if a response other than a number between 8 and 128 is given
     var length = "";
-    while (length === "" || length === null) {
+    while (length === "" || length === null || length < 8 || length > 128) {
       var length = window.prompt("How long would you like your password to be? Please enter the desired number of characters. Please enter a value of at least 8 characters, and no more than 128 characters.");
     }
 
@@ -21,9 +22,9 @@ var promptCriteria = function() {
 
   // Prompt uppercase, lowercase, or both
   var promptedUpperOrLower = function() {
-    // keep prompting if a null response is given
+    // keep prompting if a response other than 1, 2, or 3 is given
     var caseType = "";
-    while (caseType === "" || caseType === null) {
+    while (caseType === "" || caseType === null || caseType < 1 || caseType > 3) {
       var caseType = window.prompt("Would you like to include uppercase letters, lowercase letters, or both? Please enter 1 for uppercase, 2 for lowercase, or 3 for both.");
     }
 
@@ -32,9 +33,9 @@ var promptCriteria = function() {
 
   // Prompt numbers, special characters, both, or neither
   var promptedOther = function() {
-    // keep prompting if a null response is given
+    // keep prompting if a response other than 1, 2, 3, or 4 is given
     var other = "";
-    while (other === "" || other === null) {
+    while (other === "" || other === null || other < 1 || other > 4) {
       var other = window.prompt("Would you like to include numbers and/or special characters(!, $, etc.)? Please enter 1 for numbers, 2 for special characters, 3 for both, or 4 for neither.");
     }
 
