0708fa4c0e20cf12e9e880e6282346afeb5bccf6
Jaxon Adams <jadamsresume1@gmail.com>
2022-01-26 12:26:47 -0700

    added prompts for criteria

---
+++ Develop/script.js
@@ -0,0 +1,68 @@
+// When Button is Pressed
+  // Prompt Criteria
+      // Choose length between 8 and 128 characters
+      // Choose character types: uppercase, lowercase, numeric, and/or special characters
+  // Generate Password Based on Criteria
+  // Display Password
+
+var promptCriteria = function() {
+  window.alert("Please select the criteria you would like to meet for this password.");
+
+  // Prompt Length
+  var promptedLength = function() {
+    // Prompt length, and keep prompting if a null response is given
+    var length = "";
+    while (length === "" || length === null) {
+      var length = window.prompt("How long would you like your password to be? Please enter the desired number of characters. Please enter a value of at least 8 characters, and no more than 128 characters.");
+    }
+
+    return length;
+  }
+
+  // Prompt uppercase, lowercase, or both
+  var promptedUpperOrLower = function() {
+    // keep prompting if a null response is given
+    var caseType = "";
+    while (caseType === "" || caseType === null) {
+      var caseType = window.prompt("Would you like to include uppercase letters, lowercase letters, or both? Please enter 1 for uppercase, 2 for lowercase, or 3 for both.");
+    }
+
+    return caseType;
+  }
+
+  // Prompt numbers, special characters, both, or neither
+  var promptedOther = function() {
+    // keep prompting if a null response is given
+    var other = "";
+    while (other === "" || other === null) {
+      var other = window.prompt("Would you like to include numbers and/or special characters(!, $, etc.)? Please enter 1 for numbers, 2 for special characters, 3 for both, or 4 for neither.");
+    }
+
+    return other;
+  }
+
+  // Store password criteria in an object titled passwordCriteria
+  var passwordCriteria = {
+    length: promptedLength(),
+    upperOrLowerCase: promptedUpperOrLower(),
+    other: promptedOther()
+  }
+
+  // Check stored values
+  console.log(passwordCriteria);
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", promptCriteria); // temporarily changed writePassword to promptCriteria
