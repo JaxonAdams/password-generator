eed64e11c75977b060f11c550667d0dacb137373
Jaxon Adams <jadamsresume1@gmail.com>
2022-01-26 15:39:16 -0700

    added arrays for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,25 @@
   // Generate Password Based on Criteria
   // Display Password
 
+// lowercase array
+var lowercase = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+
+// uppercase array
+var uppercase = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
+// number array
+var num = [
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
+
+// special character array
+var special = [
+  " ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"
+];
 
 var promptCriteria = function() {
   window.alert("Please select the criteria you would like to meet for this password.");
